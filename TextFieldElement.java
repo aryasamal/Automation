4956a78b39cbcfeb1140e6f01b324c607d198a04
aryasamal <51173496+aryasamal@users.noreply.github.com>
2019-05-30 22:19:02 +0530

    commit TextFieldElement.java
    commit TextFieldElement.java

---
+++ TextFieldElement.java
@@ -0,0 +1,34 @@
+package com.seleniumtests.webelements;
+
+import org.openqa.selenium.By;
+
+import com.seleniumtests.core.TestLogging;
+
+public class TextFieldElement extends HtmlElement {
+    public TextFieldElement(final String label, final By by) {
+        super(label, by);
+    }
+
+    public void clear() {
+        TestLogging.logWebStep("Remove data From " + toHTML(), false);
+        findElement();
+        if (!element.getAttribute("type").equalsIgnoreCase("file")) {
+            element.clear();
+        }
+    }
+
+    public void sendKeys(final String keysToSend) {
+        TestLogging.logWebStep("Enter data: \"" + keysToSend + "\" on " + toHTML(), false);
+        findElement();
+        element.sendKeys(keysToSend);
+    }
+
+    public void type(final String keysToSend) {
+        sendKeys(keysToSend);
+    }
+
+    public void clearAndType(final String keysToSend) {
+        clear();
+        type(keysToSend);
+    }
+}
