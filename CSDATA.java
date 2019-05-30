501e1b85680ce4728ae2dc3ba54c3d85cd75cb02
aryasamal <51173496+aryasamal@users.noreply.github.com>
2019-05-30 21:26:31 +0530

    commit CSDATA.java
    commit CSDATA.java

---
+++ CSDATA.java
@@ -0,0 +1,41 @@
+package com.seleniumtests.dataobject;
+
+
+
+public class CSDATA 
+{
+	public String firstName;
+	public String lastName;
+	private String loginPassWord;
+  private String email;
+
+
+
+
+public String getfirstName() {
+		return firstName;
+	}
+	
+	public void setfirstName(final String firstName) {
+		this.firstName = firstName;
+	}
+	public String getlastName() {
+		return lastName;
+	}
+	public void setlastName(final String lastName) {
+		this.lastName = lastName;
+	}
+  public String getEmail() {
+		return email;
+	}
+	public void setEmail(final String email) {
+		this.email = email;
+	}
+	public String getLoginPassWord() {
+		return loginPassWord;
+	}
+	public void setLoginPassWord(final String loginPassWord) {
+		this.loginPassWord = loginPassWord;
+	}
+
+	
