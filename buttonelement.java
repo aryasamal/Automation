403f4639287a862ffe9fa23aac0681d2ccd7b71e
aryasamal <51173496+aryasamal@users.noreply.github.com>
2019-05-30 22:14:56 +0530

    commit buttonelement.java
    commit buttonelement.java

---
+++ buttonelement.java
@@ -0,0 +1,34 @@
+package com.seleniumtests.webelements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+import com.seleniumtests.core.TestLogging;
+
+import com.seleniumtests.driver.BrowserType;
+import com.seleniumtests.driver.WebUIDriver;
+
+public class ButtonElement extends HtmlElement {
+
+    public ButtonElement(final String label, final By by) {
+        super(label, by);
+    }
+
+    @Override
+    public void click() {
+        TestLogging.logWebStep("click on " + toHTML(), false);
+
+        BrowserType browser = WebUIDriver.getWebUIDriver().getConfig().getBrowser();
+        if (browser == BrowserType.InternetExplore) {
+            super.sendKeys(Keys.ENTER);
+        } else {
+            super.click();
+        }
+    }
+
+    public void submit() {
+        TestLogging.logWebStep("Submit form by clicking on " + toHTML(), false);
+        findElement();
+        element.submit();
+    }
+}
